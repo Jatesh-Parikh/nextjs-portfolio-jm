bf872cf950f0927c75979068d1bb188fdc4230f8
Jatesh Parikh <jateshparikh97@gmail.com>
2024-05-09 12:14:19 +0530

    projects page completed using sanity.io

---
+++ app/projects/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { ProjectsCard } from "../lib/interface";
+import { client } from "../lib/sanity";
+
+async function getData() {
+  const query = `*[_type == 'project'] {
+    link,
+    description,
+    tags,
+    "imageUrl": image.asset->url
+  }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function ProjectsPage() {
+  const data: ProjectsCard[] = await getData();
+  return (
+    <section className="max-w-7xl mx-auto w-full px-4 md:px-8">
+      <h1 className="text-4xl font-semibold lg:text-5xl pt-5">Projects</h1>
+      <p className="mt-2 leading-7 text-muted-foreground">
+        Explore the projects I&apos;ve created
+      </p>
+      <div className="py-12 grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+        {data.map((item) => (
+          <a
+            href={item.link}
+            key={item._id}
+            target="_blank"
+            className="group block0"
+          >
+            <div className="aspect-w-16 aspect-h-9 overflow-hidden rounded-2xl relative">
+              <Image
+                src={item.imageUrl}
+                alt="Hero Image"
+                fill
+                className="object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out rounded-2xl"
+              />
+            </div>
+            <div className="mt-4">
+              <h2 className="font-medium text-lg hover:underline">
+                {item.title}
+              </h2>
+              <p className="mt-1 text-muted-foreground line-clamp-3">
+                {item.description}
+              </p>
+              <div className="mt-3 flex flex-wrap gap-2">
+                {item.tags.map((tagItem, index) => (
+                  <span
+                    key={index}
+                    className="inline-flex items-center rounded-md bg-primary/10 px-3 py-1.5 text-xs sm:text-sm font-medium text-primary ring-2 ring-inset ring-primary/20"
+                  >
+                    {tagItem}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '38o7tdh5',
+    dataset: 'production'
+  }
+})
